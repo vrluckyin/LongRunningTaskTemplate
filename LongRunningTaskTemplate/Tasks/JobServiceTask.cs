2de99c644837db10f0bf401df3b159ae527189d0
Vipul Patel <vipul.patel@aisoftwarellc.com>
2021-04-02 17:09:13 +0530

    implemented lazy to make sure un-necessary scopes won't be created

--- LongRunningTaskTemplate/Tasks/JobServiceTask.cs
+++ LongRunningTaskTemplate/Tasks/JobServiceTask.cs
@@ -13,23 +13,33 @@ namespace LongRunningTaskTemplate
         protected string JobTaskName { get; set; }
         private static readonly SemaphoreSlim _lock = new SemaphoreSlim(1, 1);
         private IServiceScopeFactory _serviceScopeFactory { get; set; }
-        private IServiceScope _serviceScope { get; set; }
+        private Lazy<IServiceScope> _serviceScope { get; set; }
 
-        protected ILogger<JobServiceTask> Logger { get; private set; }
+        private ILogger<JobServiceTask> _logger = null;
+        protected ILogger<JobServiceTask> Logger
+        {
+            get
+            {
+                return _logger ?? ServiceProvider.GetService<ILogger<JobServiceTask>>();
+            }
+        }
         protected IServiceProvider ServiceProvider
         {
             get
             {
-                return _serviceScope.ServiceProvider;
+                return _serviceScope.Value.ServiceProvider;
             }
         }
 
         public JobServiceTask(IServiceScopeFactory serviceScopeFactory)
         {
             _serviceScopeFactory = serviceScopeFactory;
-            _serviceScope = _serviceScopeFactory.CreateScope();
-            Logger = ServiceProvider.GetService<ILogger<JobServiceTask>>();
+            _serviceScope = new Lazy<IServiceScope>(() =>
+            {
+                return _serviceScopeFactory.CreateScope();
+            });
         }
+
         public abstract Task<Dictionary<string, object>> Execute();
 
         public async Task<Dictionary<string, object>> Run()
@@ -55,7 +65,10 @@ namespace LongRunningTaskTemplate
                 }
                 finally
                 {
-                    _serviceScope.Dispose();
+                    if (_serviceScope.IsValueCreated)
+                    {
+                        _serviceScope.Value.Dispose();
+                    }
                 }
             }
             else
